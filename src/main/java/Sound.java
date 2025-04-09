5f0fde336c8ac9c981c8dd034a96b00e9063315c
jamestruuong <jatruong2@gmail.com>
2025-04-09 12:13:36 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -20,12 +20,12 @@ public class Sound
       if(samples[i] > limit){
         samples[i] = limit;
         count++;
+      }
     }else if(samples[i]< (-1)*limit){
         samples[i] = (-1)*limit;
         count++;
     }
     return count;
-    }
   }
 
 
