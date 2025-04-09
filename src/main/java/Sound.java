936a80ca8323000e2757c213daf8519cd98b58ea
jamestruuong <jatruong2@gmail.com>
2025-04-09 12:03:52 -0700

    Update Sound.java

--- src/main/java/Sound.java
+++ src/main/java/Sound.java
@@ -15,7 +15,7 @@ public class Sound
    */
   public int limitAmplitude(int limit)
   {  
-    int count = 0l
+    int count = 0;
     for(int i = 0; i < samples.length; i ++){
       if(samples[i] > limit){
         samples[i] = limit;
